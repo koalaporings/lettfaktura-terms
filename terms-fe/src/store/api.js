a9fbb026280c2d80fbfb92f9320359e9cadc1222
Kyle <kalan.manayon@gmail.com>
2025-09-03 22:18:57 +0800

    change baseURL

--- terms-fe/src/store/api.js
+++ terms-fe/src/store/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://lettfaktura-terms-3r4w.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
